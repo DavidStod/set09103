940d87b8a9e9be412bab441bf6924b95412c0d18
David Stoddart <40209068@live.napier.ac.uk>
2017-10-12 12:02:13 +0100

    forcing errors(working)

--- testing_test.py
+++ testing_test.py
@@ -1,9 +1,12 @@
+#change line above to whats commented to get different result
 import unittest
-import fail_test #file name your gonna test
+import fail_test
+#import pass_test
 
 class TestingTest(unittest.TestCase):
     def test_root(self):
-        self.app = pass_test.app.test_client()
+        self.app = fail_test.app.test_client()
+        #self.app = pass_test.app.test_client()
         out = self.app.get('/')
         assert '200 OK' in out.status
         assert 'charset=utf-8' in out.content_type
